07bbd537cc45c58d695cd08ae6a9744fa24aa558
Thomason Zhou <thomason.zhou@uwaterloo.ca>
2026-02-12 01:25:21 -0500

    IMU and map

--- WASM/core/telemetry.cpp
+++ WASM/core/telemetry.cpp
@@ -23,8 +23,8 @@ void log_imu(std::mutex& m_imu, const sensors::IMUData& imu_copy, double& last_i
   // std::cout << "T:" << imu_copy.timestamp << " acc:" << imu_copy.acc_x << "," << imu_copy.acc_y << "," << imu_copy.acc_z << std::endl
   // << "T:" << imu_copy.timestamp << " gyro:" << imu_copy.gyro_x << "," << imu_copy.gyro_y << "," << imu_copy.gyro_z <<
   // std::endl;
-  // std::this_thread::sleep_for(std::chrono::milliseconds(IMUIntervalMs));
 }
+
 // log sensors without significant delays in processing
 void log_lc(std::mutex& m_lc, const sensors::LidarCameraData& lc_data, double& last_lc_timestamp){
   std::cout << std::fixed << std::setprecision(5);
